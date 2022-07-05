182851cd02a7d4719c8b5417580780905b56d36d
gamejunkiez <queenangrylobster@gmail.com>
2022-07-05 09:51:32 -0500

    completed exercise

---
+++ StaticExercise/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempinFahrenheit)
+        {
+            return (tempinFahrenheit - 32) * .5556;
+        }
+
+        public static double CelsiusToFahrenheit(double tempinCelsius)
+        {
+            return tempinCelsius * 1.8 + 32;
+        }
+
+
+    }
+}
